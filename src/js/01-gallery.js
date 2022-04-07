2fd39ee7c9bdaeeeb585bdee65cbeedaaf91bd66
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-04-07 17:34:10 +0300

    exercise1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeImageEl = galleryItems.map(({ preview, original, description }) => 
+    
+    `<li> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> </li>`
+   
+).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', makeImageEl);
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+gallery.on('show.simplelightbox');
+
+
 console.log(galleryItems);
