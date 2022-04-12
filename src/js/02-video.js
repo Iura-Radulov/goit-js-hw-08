6080bdfce8345ad7811e5e6743937273d6c97242
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-04-12 22:08:04 +0300

    exercise3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+
+    const player = new Player(iframe);
+    
+let currentTime = 3;
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+        // console.log(data.seconds);
+        // throttleTime = throttle(data.seconds, 1000);
+        
+        localStorage.setItem("videoplayer-current-time", data.seconds);
+        const currentTime = localStorage.getItem("videoplayer-current-time");
+        // console.log(currentTime);
+        // console.log(throttleTime);
+       
+        return currentTime;
+    };
+
+
+player.on('timeupdate', onPlay);
+
+
+
+console.log(currentTime);
+ player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require('lodash.throttle');
 
+const formEl = document.querySelector(".feedback-form");
+const messageEl = formEl.querySelector("textarea");
+const formData = {};
+
+formEl.addEventListener('input', throttle(onTextareaInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+populateTextarea()
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    console.log(JSON.parse(savedMessage));
+    localStorage.removeItem("feedback-form-state");
+    
+}
+
+function onTextareaInput(evt) {
+    formData[evt.target.name] = evt.target.value;    
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    if (savedMessage) {
+        messageEl.value = savedMessage;
+    }
+}
